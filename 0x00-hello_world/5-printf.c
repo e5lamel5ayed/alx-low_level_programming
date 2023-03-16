50bdb44b9a5c128ae2fa035c4d889d1a08a7d7c9
e5lamel5ayed <eslam25elsayed12@gmail.com>
2023-03-16 13:27:57 -0700

    eslam elsayed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-
 /**
- * main - Eslam elsayed hamed mohamed 
- * Return: Always 0 (Success) 
- *
+ * main - Eslam elsayed hamed mohamed
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
